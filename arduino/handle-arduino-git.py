2a6928025e2818e1bc2285d5150e8a0b60be8307
root <isuciu0@uoc.edu>
2021-02-16 13:54:40 +0000

    correct paths and working code for remote flashing of arduino

--- arduino/handle-arduino-git.py
+++ arduino/handle-arduino-git.py
@@ -7,18 +7,23 @@ logging.basicConfig(level=logging.INFO)
 
 #try to Download repo, making sure that first time ever, arduino is flashed
 try:
-	git.Git("/home/uoc/UOC/Mainflux_Stuff/ADO-mainflux/testing_GitPython/CheckDL").clone("https://github.com/wine-uoc/ADO.git")
-	subprocess.call("/home/uoc/UOC/Mainflux_Stuff/ADO-mainflux/testing_GitPython/flash-arduino")
+	git.Git("/opt/Arduino").clone("https://github.com/wine-uoc/ADO-Arduino.git")
+	git_dir = "/opt/Arduino/ADO-Arduino"
+	g = git.cmd.Git(git_dir)
+	g.checkout("DEMO")
+	status=g.pull('origin', 'DEMO')
+	logging.info("%s", status)
+	subprocess.call("/opt/ADO-RPi/arduino/flash-arduino")
 	logging.info("Arduino code was compiled and uploaded")
 except Exception as e:
 	logging.info("%s",str(e))
-	if "'ADO' already exists" in str(e):
+	if "already exists" in str(e):
 		logging.info("We have already downloaded this git repo")
 		logging.info("Checking if there is anything to pull")
 
-		git_dir = "/home/uoc/UOC/Mainflux_Stuff/ADO-mainflux/testing_GitPython/ADO"
+		git_dir = "/opt/Arduino/ADO-Arduino"
 		g = git.cmd.Git(git_dir)
-		print(g.checkout("DEMO"))
+		g.checkout("DEMO")
 		status=g.pull('origin', 'DEMO')
 		logging.info("%s", status)
 		if "Already up to date" in str(status):
