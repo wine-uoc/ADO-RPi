e5034e1fb376ecd42bb77dd55c5962b050683217
root <isuciu0@uoc.edu>
2021-02-17 11:22:43 +0000

    account for internet connection

--- arduino/handle-arduino-git.py
+++ arduino/handle-arduino-git.py
@@ -1,36 +1,54 @@
+import sys
 import git
 import logging
 import subprocess
+import  requests
+import time
+sys.path.append('/opt/Raspberry/ADO-RPi/rpiapp')
+from logging_filter import logger
 
-logging.basicConfig(level=logging.INFO)
+logging= logger
+url = "http://www.google.com"
+timeout = 5
+connection_status = False
+while connection_status == False:
+        logging.info("Waiting for internet connection")
+        try:
+                request = requests.get(url, timeout=timeout)
+                logging.info("Connected to the Internet")
+                connection_status = True
 
+        except (requests.ConnectionError, requests.Timeout) as exception:
+                logging.info("No internet connection.")
+        time.sleep(5)
 
 #try to Download repo, making sure that first time ever, arduino is flashed
 try:
-	git.Git("/opt/Arduino").clone("https://github.com/wine-uoc/ADO-Arduino.git")
-	git_dir = "/opt/Arduino/ADO-Arduino"
-	g = git.cmd.Git(git_dir)
-	g.checkout("DEMO")
-	status=g.pull('origin', 'DEMO')
-	logging.info("%s", status)
-	subprocess.call("/opt/ADO-RPi/arduino/flash-arduino")
-	logging.info("Arduino code was compiled and uploaded")
+        git.Git("/opt/Arduino").clone("https://github.com/wine-uoc/ADO-Arduino.git")
+        git_dir = "/opt/Arduino/ADO-Arduino"
+        g = git.cmd.Git(git_dir)
+        g.checkout("DEMO")
+        status=g.pull('origin', 'DEMO')
+        logging.info("%s", status)
+        subprocess.call("/opt/Raspberry/ADO-RPi/arduino/flash-arduino")
+        logging.info("Arduino code was compiled and uploaded")
 except Exception as e:
-	logging.info("%s",str(e))
-	if "already exists" in str(e):
-		logging.info("We have already downloaded this git repo")
-		logging.info("Checking if there is anything to pull")
+        logging.info("%s",str(e))
+        if "already exists" in str(e):
+                logging.info("We have already downloaded this git repo")
+                logging.info("Checking if there is anything to pull")
 
-		git_dir = "/opt/Arduino/ADO-Arduino"
-		g = git.cmd.Git(git_dir)
-		g.checkout("DEMO")
-		status=g.pull('origin', 'DEMO')
-		logging.info("%s", status)
-		if "Already up to date" in str(status):
-			logging.info("nothing to compile/upload")
-		else:
-			logging.info("proceeding to updating Arduino firmware")
-			subprocess.call("/opt/ADO-RPi/arduino/flash-arduino")
-			logging.info("END compile/upload")
+                git_dir = "/opt/Arduino/ADO-Arduino"
+                g = git.cmd.Git(git_dir)
+                g.checkout("DEMO")
+                status=g.pull('origin', 'DEMO')
+                logging.info("%s", status)
+                if "Already up to date" in str(status):
+                        logging.info("nothing to compile/upload")
+                else:
+                        logging.info("proceeding to updating Arduino firmware")
+                        subprocess.call("/opt/Raspberry/ADO-RPi/arduino/flash-arduino")
+                        logging.info("END compile/upload")
+
 
 
