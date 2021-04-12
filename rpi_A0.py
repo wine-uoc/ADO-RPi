64ec39021368ef23c0f1564a15b7538884998caf
isuciu <isuciu0@uoc.edu>
2021-04-12 15:16:54 +0200

    no need for GPIOs

--- rpi_A0.py
+++ rpi_A0.py
@@ -1,15 +1,5 @@
 """RPI to Arduino -- building interactions"""
 from rpiapp.command_manager import main
-import RPi.GPIO as GPIO
 import time
 if __name__ == "__main__":
-    GPIO.cleanup()
-    GPIO.setmode(GPIO.BCM)
-    GPIO.setup(12, GPIO.OUT, initial=GPIO.HIGH)
-    GPIO.output(12, GPIO.LOW) #turn off arduino
-    GPIO.setup(11, GPIO.OUT, initial=GPIO.HIGH)
-    GPIO.output(11, GPIO.HIGH) #disable reset pin
-    time.sleep(2)
-    GPIO.output(12, GPIO.HIGH) #turn on arduino
     main()  # put this inside a thread, runs periodically as deamon
-    # main()  # put this inside another thread
